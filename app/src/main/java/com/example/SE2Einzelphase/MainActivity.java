10fc5c5777a046f1cbfe2e79fbe1936131bc37c9
= <hans.griesser@gmail.com>
2024-03-06 22:52:18 +0100

    Rudimental implementation of task 2.1

--- app/src/main/java/com/example/SE2Einzelphase/MainActivity.java
+++ app/src/main/java/com/example/SE2Einzelphase/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.SE2Einzelphase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText studentNumber;
+    private TextView serverResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnSend = findViewById(R.id.btnSend);
+        studentNumber = findViewById(R.id.editStudentNumber);
+        serverResponse = findViewById(R.id.responseText);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSendToServer();
+            }
+        });
+    }
+
+    public void btnSendToServer() {
+        String userInput = studentNumber.getText().toString();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket("se2-submission.aau.at",20080);
+
+                    PrintWriter out = new PrintWriter(s.getOutputStream(),true);
+                    out.println(userInput);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String response = in.readLine();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverResponse.setText(response);
+                        }
+                    });
+
+
+                    out.close();
+                    in.close();
+                    s.close();
+
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
