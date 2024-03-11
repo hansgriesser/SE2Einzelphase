a74eb79291923f5437aa88abb603a1c4a229639d
= <hans.griesser@gmail.com>
2024-03-11 21:04:20 +0100

    Implementation of task 2.2

--- app/src/main/java/com/example/SE2Einzelphase/MainActivity.java
+++ app/src/main/java/com/example/SE2Einzelphase/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText studentNumber;
     private TextView serverResponse;
+    private TextView calculatedMatrNr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +36,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btnSend = findViewById(R.id.btnSend);
+        Button btnCaclulate = findViewById(R.id.btnCalculate);
+
         studentNumber = findViewById(R.id.editStudentNumber);
         serverResponse = findViewById(R.id.responseText);
+        calculatedMatrNr = findViewById(R.id.calculatedMatrNrPlaceHolder);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnSendToServer();
+            }
+        });
+
+        btnCaclulate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = studentNumber.getText().toString();
+                String res = manipulateMatrNrString(userInput);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        calculatedMatrNr.setText(res);
+                    }
+                });
             }
         });
     }
@@ -54,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Socket s = new Socket("se2-submission.aau.at",20080);
+                    Socket s = new Socket("se2-submission.aau.at", 20080);
 
-                    PrintWriter out = new PrintWriter(s.getOutputStream(),true);
+                    PrintWriter out = new PrintWriter(s.getOutputStream(), true);
                     out.println(userInput);
 
                     BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -74,11 +92,31 @@ public class MainActivity extends AppCompatActivity {
                     in.close();
                     s.close();
 
-                } catch(IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
 
+    }
+
+    // Replace every second digit with corresponding character (0 = a, 1 = b....)
+    // For example: 12209209 should return 1b2a9b0i
+    public String manipulateMatrNrString(String matrnr) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < matrnr.length(); i++) {
+            if (i % 2 != 0) {
+                // Latin Alphabet: Lowercase: a-h = 97-105 with UniCode representation
+                char digit = matrnr.charAt(i);
+                // If the digit is zero, we replace by a, otherwise we add the numeric value
+                char correspondingChar = (digit == '0') ? 'a' : (char) ('a' + Character.getNumericValue(digit) - 1);
+                sb.append(correspondingChar);
+            } else {
+                sb.append(matrnr.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
